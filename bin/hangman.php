c4be6d39f2b4e3aefaafa3ba26082c91db72b1ae
Vitaliy-Ryazin <rmvitaliy@yandex.ru>
2022-10-19 11:31:58 +0300

    4

--- bin/hangman.php
+++ bin/hangman.php
@@ -1,13 +1,18 @@
 <?php
 
-require_once(__DIR__ . "/../vendor/autoload.php");
-
-use function rmvit\hangman\Controller\key;
-
-if (isset($argv[1])) {
-    $key = $argv[1];
-    key($key);
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../vendor/autoload.php');
 } else {
-    $key = "-n";
-    key($key);
+    require_once(__DIR__ . '/../../../autoload.php');
+}
+
+use function rmvit\hangman\Controller\menu;
+use function rmvit\hangman\Controller\menuReplay;
+
+if (isset($argv[1]) && isset($argv[2])) {
+    menuReplay($argv[1], $argv[2]);
+} elseif (isset($argv[1])) {
+    menu($argv[1]);
+} else {
+    menu("-n");
 }
--- src/Controller.php
+++ src/Controller.php
@@ -2,70 +2,110 @@
 
 namespace rmvit\hangman\Controller;
 
+use function rmvit\hangman\Model\addAttemptsInfo;
 use function rmvit\hangman\View\showGame;
-use function rmvit\hangman\View\showList;
-use function rmvit\hangman\View\showReplay;
-use function rmvit\hangman\View\help;
+use function rmvit\hangman\Model\openDB;
+use function rmvit\hangman\Model\createRecord;
+use function rmvit\hangman\Model\gameList;
+use function rmvit\hangman\Model\gameReplay;
+use function rmvit\hangman\Model\updateDB;
 
-function key($key)
+function menu($key)
 {
     if ($key == "--new" || $key == "-n") {
         startGame();
     } elseif ($key == "--list" || $key == "-l") {
-        showList();
-    } elseif ($key == "--replay" || $key == "-r") {
-        showReplay();
+        gameList();
     } elseif ($key == "--help" || $key == "-h") {
-        help();
+        \cli\line("
+        --new or -n: start new game\n
+        --list or -l: list of all games\n
+        --help or -h: command list\n
+        --replay [id] or -r [id]: replay of the game");
     } else {
-        echo "Неверный ключ.";
+        \cli\line("Wrong key");
     }
+}
+
+function menuReplay($key1, $key2)
+{
+    if ($key1 == "--replay" || $key1 == "-r") {
+        if (is_numeric($key2)) {
+            gameReplay($key2);
+        } else {
+            \cli\line("Wrong key");
+        }
+    } else {
+        \cli\line("Wrong key");
+    }
+}
+
+function showResult($word, $result)
+{
+    \cli\line("$result");
+
+    \cli\line("The hidden word was: $word");
 }
 
 function startGame()
 {
-    $words = array("string", "letter", "artist", "arrive", "public");
-    $playWord = $words[array_rand($words)];
-    $remainingLetters = substr($playWord, 1, -1);
-    $maxAnswers = strlen($remainingLetters);
-    $maxFaults = 6;
+    $root = openDB();
+    date_default_timezone_set("Europe/Moscow");
+    $gameDate = date("d") . "." . date("m") . "." . date("Y");
+    $gameTime = date("H") . ":" . date("i") . ":" . date("s");
+    $nickname = getenv("username");
 
-    $progress = "______";
-    $progress[0] = $playWord[0];
-    $progress[-1] = $playWord[-1];
+    $wordBase = array("hidden", "answer", "laptop", "unreal", "script");
+    $randomChoice = random_int(0, count($wordBase) - 1);
+    $word = $wordBase[$randomChoice];
+    $lengthWord = strlen($word);
+    $remaining = $word;
 
-    $faultCount = 0;
-    $answersCount = 0;
+    $id = createRecord($gameDate, $gameTime, $nickname, $word);
 
-    do {
-        showGame($faultCount, $progress);
-        $letter = mb_strtolower(\cli\prompt("Буква"));
-        $tempCount = 0;
+    $entryField = "";
+    for ($i = 0; $i < $lengthWord; $i++) {
+        $entryField .= ".";
+    }
 
-        for ($i = 0; $i < strlen($remainingLetters); $i++) {
-            if ($remainingLetters[$i] == $letter) {
-                $progress[$i + 1] = $letter;
-                $remainingLetters[$i] = " ";
-                $answersCount++;
-                $tempCount++;
+    $fails = 0;
+    $rightAnswers = 0;
+    $progress = 0;
+
+    while ($fails != 6 && $rightAnswers != $lengthWord) {
+        showGame($fails, $entryField);
+        $letter = mb_strtolower(\cli\prompt("Letter: "));
+        $attempt = 0;
+
+        for ($i = 0; $i < strlen($remaining); $i++) {
+            if ($remaining[$i] == $letter) {
+                $entryField[$i] = $letter;
+                $remaining[$i] = " ";
+                $rightAnswers++;
+                $attempt++;
             }
         }
 
-        if ($tempCount == 0) {
-            $faultCount++;
+        if ($attempt == 0) {
+            $fails++;
+            $result = 0;
+        } else {
+            $result = 1;
         }
-    } while ($faultCount < $maxFaults && $answersCount < $maxAnswers);
-    showGame($faultCount, $progress);
-    showResult($answersCount, $playWord);
-}
+        $progress++;
 
-//результат игры
-function showResult($answersCount, $playWord)
-{
-    if ($answersCount == 4) {
-        \cli\line("Вы победили!");
-    } else {
-        \cli\line("\nВы проиграли!");
+        addAttemptsInfo($id, $progress, $letter, $result);
     }
-    \cli\line("\nИгровое слово было: $playWord\n");
+
+    $result = "";
+
+    if ($rightAnswers == $lengthWord) {
+        $result = "Win";
+    } else {
+        $result = "Lose";
+    }
+
+    showGame($fails, $entryField);
+    showResult($word, $result);
+    updateDB($id, $result);
 }
